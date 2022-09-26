6c6bb2f9ddec17421a443d294de24e3a45fcb17b
taker4 <is0663xi@ed.ritsumei.ac.jp>
2022-09-26 13:19:57 +0900

    delete blog

--- src/components/pages/contact/index.tsx
+++ src/components/pages/contact/index.tsx
@@ -1,34 +1,56 @@
-import { AiTwotoneTags } from 'react-icons/ai';
-import { Link } from '@/components/common/Link';
+
 import { MainLayout } from '@/components/features/app/Layout';
-//import { Profile } from '@/components/features/app/Profile';
+import { Profile } from '@/components/features/app/Profile';
 
-type Props = {
-  tags: string[];
-};
+export const About = () => (
+  <MainLayout
+    className="lg:block mt-n30"
+    main={
+      <>
+        <Profile />
+        <div className="school p-4 lg:p-10 bg-primary-1">
+          <h2 className="text-2xl font-bold text-primary-1 mb-6">沿革</h2>
+          <div className="tree_container">
+            <div className="timeline-item" date-is="2020">
+              <h1>Ri-one ssl</h1>
+              <h1>(small size league)発足</h1>
+              
+              
+            </div>
 
-export const Contact: React.VFC<Props> = ({ tags }) => {
-  return (
-    <MainLayout
-      className="lg:block"
-      main={
-        <div className="vstack gap-10 p-8 bg-primary-1 mt-n30">
-          <div className="center">
-            <h2 className="center gap-2 py-2 px-3 border-b-2 border-teal-800 dark:border-teal-400 text-xl font-bold text-primary-1">
-              <AiTwotoneTags />
-              タグ
-            </h2>
+            <div className="timeline-item" date-is="2021">
+              <h1>アジア・パシフィック大会</h1>
+              <p>
+                愛知（名古屋）大会 ５位入賞
+                
+              </p>
+            </div>
+            <div className="timeline-item" date-is="2022">
+              <h1>世界大会</h1>
+              <p>タイ・バンコク大会 Division B ５位</p>
+              
+              
+            </div>
+            <div className="timeline-item" date-is="2023"></div>
           </div>
 
-          <div className="wrap gap-3">
-            {tags.map((tag) => (
-              <Link key={tag} href={`/tags/${tag}`} passHref>
-                <a className="badge">{tag}</a>
-              </Link>
-            ))}
+          <h2 className="text-2xl font-bold text-primary-1 mb-6 mt-6">Event</h2>
+          <div className="tree_container">
+            <div className="timeline-item" date-is="2022"></div>
+            <div className="timeline-item" date-is="2022-8">
+              <p>
+                立命館大学オープンキャンパス・サマースクール
+                <br /> 「サッカーロボットと戦おう」
+              </p>
+            </div>
+            <div className="timeline-item" date-is="2022-9">
+              <p>
+                KC3 
+              </p>
+            </div>
           </div>
         </div>
-      }
-    />
-  );
-};
+      </>
+    }
+  />
+);
