645a1a9dcd3fba8808e0e8abde78eed4f92a7548
taker4 <is0663xi@ed.ritsumei.ac.jp>
2022-09-26 16:35:57 +0900

    a

--- src/components/pages/contact/index.tsx
+++ src/components/pages/contact/index.tsx
@@ -50,8 +50,6 @@ export const About = () => (
             </div>
           </div>
         </div>
-        <iframe title="This is contact form" />
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfMP6EbG0xmZq7D0HUWyuNjhZfDQ9iHPJg9Sd-55zPgnzbNgA/viewform?embedded=true"></iframe>
       </>
     }
   />
