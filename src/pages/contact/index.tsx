f4d2cdd96b16d26e2797f9ce92a1b27288bdea43
taker4 <is0663xi@ed.ritsumei.ac.jp>
2022-09-27 19:24:50 +0900

    ♻︎change contact

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,24 @@
-import { About } from '@/components/pages/about';
 
-const View: React.VFC = () => <About />;
+const View: React.VFC = () => 
+  <>
+  <main className="page">
+    
+    <header>
+        
+        <div className="details">
+            <h2>
+                Contact
+            </h2>
+        </div>
+    </header>
+    <article className="generated">
+        <div className="form">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfMP6EbG0xmZq7D0HUWyuNjhZfDQ9iHPJg9Sd-55zPgnzbNgA/viewform?embedded=true" width="1000" height="1154" max-width="100%">読み込んでいます…</iframe>
+        </div>
+
+    </article>
+</main>
+  </>
+;
 
 export default View;
