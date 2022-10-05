acaab9074207ad17e8b5c4ba0abbfc6e7949ea05
Mitsushimashima <is0691hf@ed.ritsumei.ac.jp>
2022-10-05 22:58:13 +0900

    ✨add:member_set

--- src/components/pages/members/index.tsx
+++ src/components/pages/members/index.tsx
@@ -9,6 +9,10 @@ export const Members = () => (
         <Profile />
         <div className="school p-4 lg:p-10 bg-primary-1">
           <h2 className="text-2xl font-bold text-primary-1 mb-6">ソフト班 Software Engineer</h2>
+          <div className="ssl_member">
+            <div className="thum"><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" alt="メンバーA" /></div>
+            <div className="name"><h1>メンバーA</h1></div>
+          </div>
           <h2 className="text-2xl font-bold text-primary-1 mb-6">ハード班 Mechanical Engineer</h2>
           <h2 className="text-2xl font-bold text-primary-1 mb-6">回路班 Electrical Engineer</h2>
         </div>
