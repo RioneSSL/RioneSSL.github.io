dde61e6dadfbc0e4dd88de6898fef1e3d85c7947
Mitsushimashima <is0691hf@ed.ritsumei.ac.jp>
2022-10-06 16:43:48 +0900

    ♻️change:members_page

--- src/components/pages/members/index.tsx
+++ src/components/pages/members/index.tsx
@@ -9,20 +9,18 @@ export const Members = () => (
         <Profile />
         <div className="school p-4 lg:p-10 bg-primary-1">
           <h2 className="text-2xl font-bold text-primary-1 mb-6">ソフト班 Software Engineer</h2>
-          <div className="ssl_member">
-            <div className="thum"><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" alt="メンバーA" /></div>
-            <div className="name"><h1>メンバーA</h1></div>
+          <div className='container-fluid'>
+            <div className = "row">
+              <div className="float-left w-25 d-inline-flex">
+                <div className="thum"><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" className='rounded' alt="メンバーA" /></div>
+                <div className='text-center'><h1>メンバー</h1></div>
+              </div>
+            </div>
           </div>
-          <h2 className="text-2xl font-bold text-primary-1 mb-6">ハード班 Mechanical Engineer</h2>
-          <div className="ssl_member">
-            <div className="thum"><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" alt="メンバーA" /></div>
-            <div className="name"><h1>メンバーB</h1></div>
-          </div>
-          <h2 className="text-2xl font-bold text-primary-1 mb-6">回路班 Electrical Engineer</h2>
-          <div className="ssl_member">
-            <div className="thum"><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" alt="メンバーA" /></div>
-            <div className="name"><h1>メンバーC</h1></div>
-          </div>
+          <h2 className="text-2xl font-bold text-primary-1 mb-6 mt-6">ハード班 Mechanical Engineer</h2>
+          
+          <h2 className="text-2xl font-bold text-primary-1 mb-6 mt-6">回路班 Electrical Engineer</h2>
+          
         </div>
       </>
     }
