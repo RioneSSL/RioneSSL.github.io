8aac52196093c88d050d21b6c0f042f2818312ac
Mitsushimashima <is0691hf@ed.ritsumei.ac.jp>
2022-10-07 22:24:47 +0900

    ♻️fix:members_list

--- src/components/pages/members/index.tsx
+++ src/components/pages/members/index.tsx
@@ -8,16 +8,42 @@ export const Members = () => (
       <>
         <Profile />
         <div className="school p-4 lg:p-10 bg-primary-1">
-          <h2 className="text-2xl font-bold text-primary-1 mb-6">ソフト班 Software Engineer</h2>
-          <div className='ssl_member'>
-            <div className='member_face'><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" className='rounded' alt="メンバーA" /></div>
-            <div className='member_name'><h1>メンバー</h1></div>
-            <div className='member_introduction'>ハード班、回路班のメンバーでもある</div>
+          <div className='ssl_team'>
+            <h2 className="text-2xl font-bold text-primary-1 mb-1">ソフト班 Software Engineer</h2>
+            <div className='ssl_members'>
+              <div className='ssl_member'>
+                <div className='member_face'><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" className='rounded' alt="メンバーA" /></div>
+                <div className='member_name'>メンバー</div>
+                <div className='member_introduction'>ハード班、回路班のメンバーでもある。あああああああああああああああああああああ</div>
+              </div>
+              <div className='ssl_member'>
+                <div className='member_face'><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" className='rounded' alt="メンバーA" /></div>
+                <div className='member_name'>メンバー</div>
+                <div className='member_introduction'>ハード班、回路班のメンバーでもある。あああああああああああああああああああああ</div>
+              </div>
+              <div className='ssl_member'>
+                <div className='member_face'><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" className='rounded' alt="メンバーA" /></div>
+                <div className='member_name'>メンバー</div>
+                <div className='member_introduction'>ハード班、回路班のメンバーでもある。あああああああああああああああああああああ</div>
+              </div>
+              <div className='ssl_member'>
+                <div className='member_face'><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" className='rounded' alt="メンバーA" /></div>
+                <div className='member_name'>メンバー</div>
+                <div className='member_introduction'>ハード班、回路班のメンバーでもある。あああああああああああああああああああああ</div>
+              </div>
+              <div className='ssl_member'>
+                <div className='member_face'><img src="https://avatars.githubusercontent.com/u/71633059?s=280&v=4" className='rounded' alt="メンバーA" /></div>
+                <div className='member_name'>メンバー</div>
+                <div className='member_introduction'>ハード班、回路班のメンバーでもある。あああああああああああああああああああああ</div>
+              </div>
+            </div>
           </div>
-          <h2 className="text-2xl font-bold text-primary-1 mb-6 mt-6">ハード班 Mechanical Engineer</h2>
-          
-          <h2 className="text-2xl font-bold text-primary-1 mb-6 mt-6">回路班 Electrical Engineer</h2>
-          
+          <div className='ssl_team'>
+            <h2 className="text-2xl font-bold text-primary-1 mb-6 mt-6 mb-6">ハード班 Mechanical Engineer</h2>
+          </div>
+          <div className='ssl_team'>
+            <h2 className="text-2xl font-bold text-primary-1 mb-6 mt-6 mb-6">回路班 Electrical Engineer</h2>
+          </div>
         </div>
       </>
     }
